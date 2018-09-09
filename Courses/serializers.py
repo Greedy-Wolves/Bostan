a629c47296ddefc1c745a1cafd5015401d94d8c5
Ehsan Aliakbar <e.aliakbar97@gmail.com>
2018-09-09 06:48:09 +0430

    change classTime serializer to show 16.5 instead of 16:30:00

--- Courses/serializers.py
+++ Courses/serializers.py
@@ -9,9 +9,20 @@ class CourseSerializer(serializers.ModelSerializer):
 
 
 class ClassTimeSerializer(serializers.ModelSerializer):
+    start = serializers.SerializerMethodField('std_start_time')
+    end = serializers.SerializerMethodField('std_end_time')
+
+    def std_start_time(self, obj):
+        hour, minute, __ = str(obj.start_time).split(":")
+        return int(hour) + int(minute)/60
+
+    def std_end_time(self, obj):
+        hour, minute, __ = str(obj.end_time).split(":")
+        return int(hour) + int(minute)/60
+
     class Meta:
         model = ClassTime
-        fields = '__all__'
+        fields = ('day_of_week', 'start', 'end')
 
 
 class OfferedCourseSerializer(serializers.ModelSerializer):
