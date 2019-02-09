b48b9df745681e48ab5eba73c11396ee94d617d9
Ehsan Aliakbar <e.aliakbar97@gmail.com>
2019-02-09 22:56:05 +0330

    push image to fandogh using travis

--- deployment/scripts/deploy-to-fandogh.sh
+++ deployment/scripts/deploy-to-fandogh.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+pip install fandogh_cli --upgrade
+fandogh login --username $FAN_USR --password $FAN_PASS
+fandogh image init --name=bostan_backend
+fandogh image publish --version `printf "v0.%03d" ${TRAVIS_BUILD_NUMBER}`
+
 echo "TRAVIS_BUILD_NUMBER: ${TRAVIS_BUILD_NUMBER}"
 echo "TRAVIS_JOB_NUMBER: ${TRAVIS_JOB_NUMBER}"
