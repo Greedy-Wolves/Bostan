20ffc562678adb6b4f5f1c87485668586e94d27f
Ehsan Aliakbar <e.aliakbar97@gmail.com>
2019-02-09 23:12:10 +0330

    bypass fandogh login questions in travis

--- deployment/scripts/deploy-to-fandogh.sh
+++ deployment/scripts/deploy-to-fandogh.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-fandogh login --username $FAN_USR --password $FAN_PASS
+COLLECT_ERROR=True fandogh login --username $FAN_USR --password $FAN_PASS
 fandogh image init --name=bostan_backend
-fandogh image publish --version `printf "v0.%03d" ${TRAVIS_BUILD_NUMBER}`
-
-echo "TRAVIS_BUILD_NUMBER: ${TRAVIS_BUILD_NUMBER}"
-echo "TRAVIS_JOB_NUMBER: ${TRAVIS_JOB_NUMBER}"
+fandogh image publish --version=$TRAVIS_COMMIT
